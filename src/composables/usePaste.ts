bbd4d6ece3286c3ba38139e7a7259ddedb719d89
calcuis <113646141+calcuis@users.noreply.github.com>
2025-02-23 17:40:15 -0800

    Add files via upload

---
+++ src/composables/usePaste.ts
@@ -0,0 +1,116 @@
+import { LiteGraph } from '@comfyorg/litegraph'
+import type { LGraphNode } from '@comfyorg/litegraph'
+import { useEventListener } from '@vueuse/core'
+
+import { app } from '@/scripts/app'
+import { useCanvasStore } from '@/stores/graphStore'
+import { useWorkspaceStore } from '@/stores/workspaceStore'
+import { ComfyWorkflowJSON } from '@/types/comfyWorkflow'
+import { isImageNode, isVideoNode } from '@/utils/litegraphUtil'
+
+/**
+ * Adds a handler on paste that extracts and loads images or workflows from pasted JSON data
+ */
+export const usePaste = () => {
+  const workspaceStore = useWorkspaceStore()
+  const canvasStore = useCanvasStore()
+
+  const pasteItemOnNode = (
+    items: DataTransferItemList,
+    node: LGraphNode | null
+  ) => {
+    if (!node) return
+
+    const blob = items[0]?.getAsFile()
+    if (!blob) return
+
+    node.pasteFile?.(blob)
+    node.pasteFiles?.(
+      Array.from(items)
+        .map((i) => i.getAsFile())
+        .filter((f) => f !== null)
+    )
+  }
+
+  useEventListener(document, 'paste', async (e: ClipboardEvent) => {
+    // ctrl+shift+v is used to paste nodes with connections
+    // this is handled by litegraph
+    if (workspaceStore.shiftDown) return
+
+    const canvas = canvasStore.canvas
+    if (!canvas) return
+
+    const graph = canvas.graph
+    // @ts-expect-error: Property 'clipboardData' does not exist on type 'Window & typeof globalThis'.
+    // Did you mean 'Clipboard'?ts(2551)
+    // TODO: Not sure what the code wants to do.
+    let data = e.clipboardData || window.clipboardData
+    const items: DataTransferItemList = data.items
+
+    const currentNode = canvas.current_node as LGraphNode
+    const isNodeSelected = currentNode?.is_selected
+
+    const isImageNodeSelected = isNodeSelected && isImageNode(currentNode)
+    const isVideoNodeSelected = isNodeSelected && isVideoNode(currentNode)
+
+    let imageNode: LGraphNode | null = isImageNodeSelected ? currentNode : null
+    const videoNode: LGraphNode | null = isVideoNodeSelected
+      ? currentNode
+      : null
+
+    // Look for image paste data
+    for (const item of items) {
+      if (item.type.startsWith('image/')) {
+        if (!imageNode) {
+          // No image node selected: add a new one
+          const newNode = LiteGraph.createNode('LoadImage')
+          // @ts-expect-error array to Float32Array
+          newNode.pos = [...canvas.graph_mouse]
+          imageNode = graph.add(newNode) ?? null
+          graph.change()
+        }
+        pasteItemOnNode(items, imageNode)
+        return
+      } else if (item.type.startsWith('video/')) {
+        if (!videoNode) {
+          // No video node selected: add a new one
+          // TODO: when video node exists
+        } else {
+          pasteItemOnNode(items, videoNode)
+          return
+        }
+      }
+    }
+
+    // No image found. Look for node data
+    data = data.getData('text/plain')
+    let workflow: ComfyWorkflowJSON | null = null
+    try {
+      data = data.slice(data.indexOf('{'))
+      workflow = JSON.parse(data)
+    } catch (err) {
+      try {
+        data = data.slice(data.indexOf('workflow\n'))
+        data = data.slice(data.indexOf('{'))
+        workflow = JSON.parse(data)
+      } catch (error) {
+        workflow = null
+      }
+    }
+
+    if (workflow && workflow.version && workflow.nodes && workflow.extra) {
+      await app.loadGraphData(workflow)
+    } else {
+      if (
+        (e.target instanceof HTMLTextAreaElement &&
+          e.target.type === 'textarea') ||
+        (e.target instanceof HTMLInputElement && e.target.type === 'text')
+      ) {
+        return
+      }
+
+      // Litegraph default paste
+      canvas.pasteFromClipboard()
+    }
+  })
+}
---
+++ src/composables/widgets/useImageUploadWidget.ts
@@ -0,0 +1,111 @@
+import type { LGraphNode } from '@comfyorg/litegraph'
+import { IComboWidget } from '@comfyorg/litegraph/dist/types/widgets'
+
+import { useNodeImage, useNodeVideo } from '@/composables/useNodeImage'
+import { useNodeImageUpload } from '@/composables/useNodeImageUpload'
+import { useValueTransform } from '@/composables/useValueTransform'
+import type { ComfyWidgetConstructor } from '@/scripts/widgets'
+import { useNodeOutputStore } from '@/stores/imagePreviewStore'
+import type { ComfyApp } from '@/types'
+import type { InputSpec, ResultItem } from '@/types/apiTypes'
+import { createAnnotatedPath } from '@/utils/formatUtil'
+import { addToComboValues } from '@/utils/litegraphUtil'
+
+const ACCEPTED_IMAGE_TYPES = 'image/png,image/jpeg,image/webp'
+const ACCEPTED_VIDEO_TYPES = 'video/webm,video/mp4'
+
+type InternalFile = string | ResultItem
+type InternalValue = InternalFile | InternalFile[]
+type ExposedValue = string | string[]
+
+const isImageFile = (file: File) => file.type.startsWith('image/')
+const isVideoFile = (file: File) => file.type.startsWith('video/')
+
+const findFileComboWidget = (node: LGraphNode, inputName: string) =>
+  node.widgets!.find((w) => w.name === inputName) as IComboWidget & {
+    value: ExposedValue
+  }
+
+export const useImageUploadWidget = () => {
+  const widgetConstructor: ComfyWidgetConstructor = (
+    node: LGraphNode,
+    inputName: string,
+    inputData: InputSpec,
+    app: ComfyApp
+  ) => {
+    const inputOptions = inputData[1] ?? {}
+    const { imageInputName, allow_batch, image_folder = 'input' } = inputOptions
+    const nodeOutputStore = useNodeOutputStore()
+
+    const isVideo = !!inputOptions.video_upload
+    const accept = isVideo ? ACCEPTED_VIDEO_TYPES : ACCEPTED_IMAGE_TYPES
+    const { showPreview } = isVideo ? useNodeVideo(node) : useNodeImage(node)
+
+    const fileFilter = isVideo ? isVideoFile : isImageFile
+    const fileComboWidget = findFileComboWidget(node, imageInputName)
+    const initialFile = `${fileComboWidget.value}`
+    const formatPath = (value: InternalFile) =>
+      createAnnotatedPath(value, { rootFolder: image_folder })
+
+    const transform = (internalValue: InternalValue): ExposedValue => {
+      if (!internalValue) return initialFile
+      if (Array.isArray(internalValue))
+        return allow_batch
+          ? internalValue.map(formatPath)
+          : formatPath(internalValue[0])
+      return formatPath(internalValue)
+    }
+
+    Object.defineProperty(
+      fileComboWidget,
+      'value',
+      useValueTransform(transform, initialFile)
+    )
+
+    // Setup file upload handling
+    const { openFileSelection } = useNodeImageUpload(node, {
+      allow_batch,
+      fileFilter,
+      accept,
+      onUploadComplete: (output) => {
+        output.forEach((path) => addToComboValues(fileComboWidget, path))
+        // @ts-expect-error litegraph combo value type does not support arrays yet
+        fileComboWidget.value = output
+        fileComboWidget.callback?.(output)
+      }
+    })
+
+    // Create the button widget for selecting the files
+    const uploadWidget = node.addWidget(
+      'button',
+      inputName,
+      'image',
+      () => openFileSelection(),
+      {
+        serialize: false
+      }
+    )
+    uploadWidget.label = 'choose file to upload'
+
+    // TODO: Explain this?
+    // @ts-expect-error LGraphNode.callback is not typed
+    // Add our own callback to the combo widget to render an image when it changes
+    const cb = node.callback
+    fileComboWidget.callback = function (...args) {
+      nodeOutputStore.setNodeOutputs(node, fileComboWidget.value)
+      if (cb) return cb.apply(this, args)
+    }
+
+    // On load if we have a value then render the image
+    // The value isnt set immediately so we need to wait a moment
+    // No change callbacks seem to be fired on initial setting of the value
+    requestAnimationFrame(() => {
+      nodeOutputStore.setNodeOutputs(node, fileComboWidget.value)
+      showPreview()
+    })
+
+    return { widget: uploadWidget }
+  }
+
+  return widgetConstructor
+}
